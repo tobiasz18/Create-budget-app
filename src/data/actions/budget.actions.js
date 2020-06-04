bfb6d98780deddcf4e20be6e2af8fc309f2ee689
tobiasz18 <tobiaszpost@gmail.com>
2020-06-04 21:22:17 +0200

    Add custom middleware

--- src/data/actions/budget.actions.js
+++ src/data/actions/budget.actions.js
@@ -1,53 +1,24 @@
 import {
   BUDGET_GET,
-  BUDGET_GET_FAILURE,
-  BUDGET_GET_REQUEST,
-  BUDGET_GET_SUCCESS,
-
   BUDGET_CATEGORIES_GET,
-  BUDGET_CATEGORIES_GET_FAILURE,
-  BUDGET_CATEGORIES_GET_REQUEST,
-  BUDGET_CATEGORIES_GET_SUCCESS
 } from 'data/constants';
 
 import API from 'data/fetch';
 
-export const fetchBudget = id => async(dispatch) => {
-  dispatch({
-    type: BUDGET_GET_REQUEST
-  })
-  
-  try {
-    const response = await API.budget.fetchBudget(id);
-    const data = await response.json();
-    
-    dispatch({
-      type: BUDGET_GET_SUCCESS,
-      payload: data
-    });
-  } catch (error) {
-    dispatch({
-      type: BUDGET_GET_FAILURE
-    });
-  }
+export const fetchBudget = id => {
+  const promise =  API.budget.fetchBudget(id);
+
+    return {
+      type: BUDGET_GET,
+      promise
+    }
 }
 
-export const fetchBudgetCategories = id => async(dispatch) => {
-  dispatch({
-    type: BUDGET_CATEGORIES_GET_REQUEST
-  })
-  
-  try {
-    const response = await API.budget.fetchBudgetedCategories(id);
-    const data = await response.json();
-    
-    dispatch({
-      type: BUDGET_CATEGORIES_GET_SUCCESS,
-      payload: data
-    });
-  } catch (error) {
-    dispatch({
-      type: BUDGET_CATEGORIES_GET_FAILURE
-    });
+export const fetchBudgetCategories = id => {
+  const promise = API.budget.fetchBudgetedCategories(id);
+
+  return {
+    type: BUDGET_CATEGORIES_GET,
+    promise
   }
 }
